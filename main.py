c36f8e8d3370b83b2a83db7df177bdc45f80b1db
Logan Fuller <logankfuller@gmail.com>
2017-11-29 15:14:25 -0800

    Fixed '--' from messing up sentence counts, added some verbose counts and added emotion_sentence counts for both texts.
    Previously, '--' would cause sentence counts to be off. If a sentence ended in "last word.--", the sentence would not be cut off at the period.

--- main.py
+++ main.py
@@ -90,37 +90,72 @@ def main():
 	# Tokenize the sentences in each text into tokenized sentences
 	#
 
-	text_one_sentences = set(nltk.sent_tokenize(open(file_one).read().replace('\n', ' ')))
-	text_two_sentences = set(nltk.sent_tokenize(open(file_two).read().replace('\n', ' ')))
+	print("Tokenizing...")
 
+	# Replace '--' with ' ' because '--' seems to break sentences.
+	text_one_sentences = set(nltk.sent_tokenize(open(file_one).read().replace('\n', ' ').replace('--', ' ')))
 	text_one_tokenized_sentences = []
-	text_two_tokenized_sentences = []
 
 	for sentence in text_one_sentences:
 		text_one_tokenized_sentences.append(nltk.word_tokenize(sentence))
 
+	###################
+
+	text_two_sentences = set(nltk.sent_tokenize(open(file_two).read().replace('\n', ' ').replace('--', ' ')))
+	text_two_tokenized_sentences = []
+
 	for sentence in text_two_sentences:
 		text_two_tokenized_sentences.append(nltk.word_tokenize(sentence))
+
+	# Print total number of sentences for each text
+	print("Sentence Count (" + file_one + "): " + str(len(text_one_sentences)))
+	print("Sentence Count (" + file_two + "): " + str(len(text_two_sentences)))
 
 	#
 	# Add each sentence with an emotion word into a new list (emotion_sentences)
 	# AKA it ignores sentences without an emotion word
 	#
 
-	emotion_sentences = defaultdict(list)
+	# Begin adding sentences from the first text
+
+	print("\nFiltering " + file_one + " emotion sentences")
+
+	emotion_sentences = {}
+	emotion_sentences[file_one] = defaultdict(list)
 
 	for sentence in text_one_tokenized_sentences:
 		for word in sentence:
 			for emotion in emotions_dict:
 				if word in emotions_dict[emotion]:
-					emotion_sentences[emotion].append(sentence)
+					emotion_sentences[file_one][emotion].append(sentence)
+
+	emotion_sentences[file_two] = defaultdict(list)
+
+	print("\nFiltering " + file_two + " emotion sentences")
+
+	for sentence in text_two_tokenized_sentences:
+		for word in sentence:
+			for emotion in emotions_dict:
+				if word in emotions_dict[emotion]:
+					emotion_sentences[file_two][emotion].append(sentence)
+
+	print(emotion_sentences.keys())
+
+	# Example to access values
+	# print(emotion_sentences[file_one]['anger'])
 
 	#
 	# For each emotion sentence, increment the counts of the emotions it belongs in
 	#
 
-	for emotion in emotion_sentences:
-		print(str(emotion) + ': ' + str(len(emotion_sentences[emotion])))
+	print("\nTypes of Sentences")
+	print("====================")
+
+	for text in emotion_sentences:
+		print("\n" + text)
+		print("-" * len(text))
+		for emotion in emotion_sentences[text]:
+			print(str(emotion) + ': ' + str(len(emotion_sentences[text][emotion])))
 
 '''
 #=============================================#
@@ -133,12 +168,12 @@ def file_input(prompt):
 	if os.path.isfile(path):
 		return path
 	else:
-		return file_name_input('Enter an existing file path: ')
+		return file_input('Enter an existing file path: ')
 
 def folder_input(prompt):
 	path = input(prompt)
 	if os.path.isdir(path):
 		return path
 	else:
-		return folder_name_input('Enter an existing folder path: ')
+		return folder_input('Enter an existing folder path: ')
 main()
